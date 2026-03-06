80270f3ee5ef1b48c352535f735bb08ff5cf3f63
Clarisse Gondim <claryssegondymp10@gmail.com>
2026-03-06 09:35:44 -0300

    Arquivo de Routes

---
+++ backend/routes/receitaRoutes.js
@@ -0,0 +1,16 @@
+// Aqui se faz as importações
+const express = require("express");
+const routes = express.Router();
+const receitaControlles = require("../controllers/receitaController");
+
+
+// GET mostra receita
+routes.get("/receitaTotal", receitaControlles.getAll);
+
+// POST inserir receita
+routes.post("/receitaCriar", receitaControlles.create);
+
+//DELETE especifico
+//routes.delete("/:id", receitaControlles.deleteId);
+
+module.exports = routes;
